637b89c2d27845393c16487c032b7433e4fd2098
Vladimir <noise123q@mail.ru>
2022-10-14 23:17:44 +0300

    add new fonts, like-counter, save state to localStorage, separate style

--- src/components/like-button/like-button.js
+++ src/components/like-button/like-button.js
@@ -1,8 +1,21 @@
-import {modal} from '../modal/modal';
+import {modal, body} from '../modal/modal';
 
 const likeBtn = document.querySelector('.like-button');
+const likeCounter = document.querySelector('.like-counter__number');
 
 likeBtn.addEventListener('click', function(){
     likeBtn.setAttribute('disabled','');
     modal.classList.add('show');
+    body.classList.add('scroll-blocked')
+    let result;
+    result = likeCounter.innerHTML;
+    +result++
+    likeCounter.innerHTML = result;
+    localStorage.setItem('likeCount', result)
+})
+document.addEventListener('DOMContentLoaded', function(){
+    if (localStorage.getItem('likeCount', 'result')) {
+        likeCounter.innerHTML = localStorage.getItem('likeCount', 'result');
+        likeBtn.setAttribute('disabled','');
+    }
 })
--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -1,19 +1,24 @@
 const btnModalClose = document.querySelector('.modal-close');
+const body = document.querySelector('body');
+const modal = document.querySelector('.modal');
+export {modal, body}
 
 btnModalClose.addEventListener('click', btnCloseModal);
 
 function btnCloseModal () {
     modal.classList.remove('show');
+    body.classList.remove('scroll-blocked');
 }
 
 
-const modal = document.querySelector('.modal');
-export {modal}
+
+
 
 modal.addEventListener('click', closeWithoutModalWindow);
 
 function closeWithoutModalWindow (evt) {
     if (!evt.target.closest('.modal-window')){
         modal.classList.remove('show');
+        body.classList.remove('scroll-blocked');
     }
 }
